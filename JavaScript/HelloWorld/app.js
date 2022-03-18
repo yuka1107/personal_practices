1c699f0a323fa29c63124de4a9e901d102dd165f
yuka1107 <93237466+yuka1107@users.noreply.github.com>
2022-03-18 17:00:13 +0900

    条件分岐

--- JavaScript/HelloWorld/app.js
+++ JavaScript/HelloWorld/app.js
@@ -1,3 +1,10 @@
-let str1 = ('Hello');
-let str2 = ('World!!');
-alert(str1 + str2);
+let orange = 100;
+let apple = 120;
+
+if(orange < apple){
+  alert('みかんの値段がりんごより安い');
+} else if(orange == apple){
+  alert('みかんとりんごが同じ値段');
+} else{
+  alert('みかんの値段がりんごより高い')
+}
