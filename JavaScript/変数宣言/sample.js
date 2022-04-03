94edb268d2cd673b516234ab2fbeb947f9f5fae3
yuka1107 <93237466+yuka1107@users.noreply.github.com>
2022-04-03 21:08:07 +0900

    巻き上げ

--- JavaScript/変数宣言/sample.js
+++ JavaScript/変数宣言/sample.js
@@ -1,5 +1,9 @@
-for (let i = 0; i < 10; i++) {
-  console.log(i)
+var str = "webcamp"
+
+function foo() {
+  console.log(str)
+  var str = "dmm webcamp"
+  console.log(str)
 }
 
-console.log(i)
+foo()
