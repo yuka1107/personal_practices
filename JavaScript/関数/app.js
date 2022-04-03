35fb9cbca1bf0eec36ab79abf34416a5ac112964
yuka1107 <93237466+yuka1107@users.noreply.github.com>
2022-04-03 22:39:26 +0900

    作成した関数した関数を呼び出す

--- JavaScript/関数/app.js
+++ JavaScript/関数/app.js
@@ -1,4 +1,12 @@
-function addString(strA) {
+// メイン部分
+let alertString;
+alertString = addString("WebCamp");
+
+// 作成した関数を呼び出す
+alert(alertString);
+
+// 作成した関数
+function addString(strA){
   let addStr = "Hello" + strA;
   return addStr;
 }
