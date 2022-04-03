d41f0ceac570a271ffeb03e6a538761bec3c32aa
yuka1107 <93237466+yuka1107@users.noreply.github.com>
2022-04-03 20:53:24 +0900

    varによるスコープ

--- JavaScript/変数宣言/sample.js
+++ JavaScript/変数宣言/sample.js
@@ -1,9 +1,9 @@
-// constによる再代入
-const nickname = "taro"
-console.log(nickname)
-nickname = "Jiro"
-console.log(nickname)
+var str = "webcamp" //グローバルスコープ
 
-// 「Uncaught TypeError: Assignment to constant variable. at sample.js:4」というエラー
-// エラーの内容は定数とした変数には割り当てできないというもの
-// 再代入はvarとletで可能。constでは再代入ができない。
+function foo() {
+  console.log(str)
+  var y = "hello" //関数スコープ
+}
+
+foo()
+console.log(y)
