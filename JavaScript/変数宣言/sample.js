56b613ebe706302cf3d9fafbce6ca2223d7fe6cf
yuka1107 <93237466+yuka1107@users.noreply.github.com>
2022-04-03 20:48:17 +0900

    constによる再代入

--- JavaScript/変数宣言/sample.js
+++ JavaScript/変数宣言/sample.js
@@ -1,8 +1,9 @@
-// letによる再代入
-let nickname = "taro"
+// constによる再代入
+const nickname = "taro"
 console.log(nickname)
 nickname = "Jiro"
 console.log(nickname)
 
-// 変数に値を代入した後で、別の値を代入することを再代入と言う。
+// 「Uncaught TypeError: Assignment to constant variable. at sample.js:4」というエラー
+// エラーの内容は定数とした変数には割り当てできないというもの
 // 再代入はvarとletで可能。constでは再代入ができない。
